01ab6eb93b0c6588e4cb7e45252661f4d1cba25f
itsalicelee <alice668149@gmail.com>
2024-03-13 21:37:39 -0700

    update 6.2 and 9.1

--- 6.2/main.py
+++ 6.2/main.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn import tree
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.utils import Bunch
 
 def classify(dataset, name):
-    dataset = load_breast_cancer(as_frame=True)
     X, y = dataset.data, dataset.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -18,7 +18,7 @@ def classify(dataset, name):
     acc = accuracy_score(y_test, y_pred)
     plt.figure(figsize=(20, 12))
     tree.plot_tree(clf, fontsize=10)
-    plt.title("{} Decision Tree Classifier\nAcc: {:.2f}".format(name, acc), fontsize = 28)
+    plt.title("{} Decision Tree Classifier\nAcc: {:.3f}".format(name, acc), fontsize = 28)
     plt.savefig('{}.1.png'.format(name.strip().lower()))
     print('Decision tree acc: {}'.format(acc))
 
@@ -29,15 +29,26 @@ def classify(dataset, name):
     acc = accuracy_score(y_test, y_pred)
     plt.figure(figsize=(20, 12))
     tree.plot_tree(clf, fontsize=10)
-    plt.title("{} Decision Tree Classifier with max_depth=2\nAcc: {:.2f}".format(name, acc), fontsize=28)
+    plt.title("{} Decision Tree Classifier with max_depth=2\nAcc: {:.3f}".format(name, acc), fontsize=28)
     plt.savefig('{}.2.png'.format(name.strip().lower()))
     print('Decision tree (max depth=2) acc: {}'.format(acc))
 
 if __name__ == '__main__':
-    classify(load_breast_cancer(), 'Breast Cancer')
-    classify(load_digits(), 'Digits')
-    # random dataset 
-    data = np.random.rand(15,200)
-    target = np.random.randint(2, size=data.shape[1])
-    classify(pd.DataFrame(data), 'Random')
+    classify(load_breast_cancer(as_frame=True), 'Breast Cancer')
+    classify(load_digits(as_frame=True), 'Digits')
+    # random dataset 1
+    data = np.random.rand(200,15)
+    target = np.random.randint(2, size=data.shape[0])
+    dataset = Bunch(data=pd.DataFrame(data), target=target)
+    classify(dataset, 'Random 1 Dataset (200 samples, 15 features)')
+    # random dataset 2
+    data = np.random.rand(200,5)
+    target = np.random.randint(2, size=data.shape[0])
+    dataset = Bunch(data=pd.DataFrame(data), target=target)
+    classify(dataset, 'Random 2 Dataset (200 samples, 5 features)')
+    # random dataset 3
+    data = np.random.rand(20, 15)
+    target = np.random.randint(2, size=data.shape[0])
+    dataset = Bunch(data=pd.DataFrame(data), target=target)
+    classify(dataset, 'Random 3 Dataset (20 features, 15 samples)')
     
--- 9.1/main.py
+++ 9.1/main.py
@@ -5,79 +5,139 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-class Classifier(nn.Module):
-	def __init__(self):
-		super(Classifier, self).__init__()
-		self.cnn = nn.Sequential(
-			nn.Conv2d(1, 32, kernel_size=3, padding=1),
-			nn.ReLU(),
-			nn.MaxPool2d(kernel_size=2, stride=2),
-			nn.Conv2d(32, 64, kernel_size=3, padding=1),
-			nn.ReLU(),
-			nn.MaxPool2d(kernel_size=2, stride=2),
-			nn.Conv2d(64, 64, kernel_size=3, padding=1),
-			nn.ReLU(),
-			nn.MaxPool2d(kernel_size=2, stride=2)
+
+def computeMEC(data):
+    mec = 0
+    n_size = len(data)
+    for i in range(10):
+        p = data.count(i) / n_size
+        mec += - data.count(i) * (np.log2(p))
+    return mec 
+    
+# MEC  = 785*128 +128 + 64 + 10
+class Classifier1(nn.Module):
+    def __init__(self):
+        super(Classifier1, self).__init__()
+        self.fc = nn.Sequential(
+            
+            nn.Linear(28*28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
         )
-		
-		self.flatten = nn.Flatten()
-		self.fc = nn.Sequential(
-			nn.Linear(64 * 3 * 3, 64),
-			nn.ReLU(),
-			nn.Linear(64, 10)		
-		)
 
-	def forward(self, x):
-		x = self.cnn(x)
-		x = self.flatten(x)
-		x = self.fc(x)
-		return x
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
 
-def train(epochs):
-	for epoch in range(epochs):
-		for inputs, labels in tqdm(train_loader):
-			optimizer.zero_grad()
-			outputs = model(inputs)
-			loss = criterion(outputs, labels)
-			loss.backward()
-			optimizer.step()
+# MEC  = 785*64 + 64 + 10
+class Classifier2(nn.Module):
+    def __init__(self):
+        super(Classifier2, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(28*28, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+# MEC  = 785*256 + 256 + 64 + 10
+class Classifier3(nn.Module):
+    def __init__(self):
+        super(Classifier3, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(28*28, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+def train(epochs, model, model_name):
+    losses = []
+    for epoch in range(epochs):
+        l = []
+        for inputs, labels in tqdm(train_loader):
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            l.append(loss.item())
+        losses.append(np.mean(l))
+    return losses         
 
 def eval():
-	model.eval()
-	all_predictions = []
-	all_labels = []
+    model.eval()
+    all_predictions = []
+    all_labels = []
 
-	with torch.no_grad():
-		for inputs, labels in tqdm(test_loader):
-			outputs = model(inputs)
-			predictions = torch.argmax(outputs, dim=1)
-			all_predictions.extend(predictions.cpu().numpy())
-			all_labels.extend(labels.cpu().numpy())
-	
-	return all_predictions, all_labels
+    with torch.no_grad():
+        for inputs, labels in tqdm(test_loader):
+            outputs = model(inputs)
+            predictions = torch.argmax(outputs, dim=1)
+            all_predictions.extend(predictions.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+    
+    return all_predictions, all_labels
 
 if __name__ == '__main__':
-	transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-	train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
-	test_dataset = datasets.MNIST('./data', train=False, download=True, transform=transform)
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('./data', train=False, download=True, transform=transform)
+    
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    targets = []
+    for data, target in train_dataset:
+        targets.append(target)
+    
+    mec = computeMEC(targets)
+    
+    model_losses = []
+    model_names = ['Classifier1', 'Classifier2', 'Classifier3']
+    models = [Classifier1(), Classifier2(), Classifier3()]
+    for i in range(3):
+        model = models[i]
+        criterion = nn.CrossEntropyLoss()
+        lr = 0.0001
+        optimizer = optim.Adam(model.parameters(), lr=lr)
 
-	train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-	test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+        # Training the model
+        print("Training Model{}".format(i + 1))
+        epochs = 50
+        loss = train(epochs, model, model_names[i])
+        model_losses.append(loss)
+    
+        # Evaluate the model on the test set
+        print("Evaluating Model{}".format(i + 1))
+        all_predictions, all_labels = eval()
+        accuracy = accuracy_score(all_labels, all_predictions)
+        print("Test Accuracy Model{}: {:.3f}".format(i + 1, accuracy))
+loss1, loss2, loss3 = model_losses[0], model_losses[1], model_losses[2]
+plt.figure(figsize=(8, 6))
+plt.plot(range(epochs), loss1, label='Classifier1')
+plt.plot(range(epochs), loss2, label='Classifier2')
+plt.plot(range(epochs), loss3, label='Classifier3')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Loss vs Epochs for three Models')
+plt.legend()
+plt.savefig('9.1.png')
 
-	model = Classifier()
-	criterion = nn.CrossEntropyLoss()
-	lr = 0.0001
-	optimizer = optim.Adam(model.parameters(), lr=lr)
-
-	# Training the model
-	print("Training the model")
-	epochs = 5
-	train(epochs=epochs)
-	
-	# Evaluate the model on the test set
-	print("Evaluating the model")
-	all_predictions, all_labels = eval()
-	accuracy = accuracy_score(all_labels, all_predictions)
-	print(f"Test Accuracy: {accuracy}")
+    
